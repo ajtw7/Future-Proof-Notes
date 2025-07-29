a21685d2ffe6be8ff334183cc6fad8115622244f
ajtw7 <andrew.worsley91@gmail.com>
2025-07-29 13:21:16 -0400

    WIP: Scaffolding of basic Note class v2: Declaring class attr .

--- python/notes-manager/note.py
+++ python/notes-manager/note.py
@@ -5,6 +5,13 @@ import yaml
 class Note:
     def __init__(self, title: str, content: str, tags: Optional[List[str]] = None, created: Optional[datetime] = None,
                  author: Optional[str] = None, status: Optional[str] = None, priority: Optional[str] = None):
+        self.title = title
+        self.content = content
+        self.tags = tags if tags is not None else []
+        self.created = created if created is not None else datetime.now()
+        self.author = author
+        self.status = status
+        self.priority = priority
         
 
 
