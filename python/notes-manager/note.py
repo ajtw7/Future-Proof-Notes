f3f31decd6ffc2073e28b00ddba997585602e3eb
ajtw7 <andrew.worsley91@gmail.com>
2025-07-29 12:02:26 -0400

    WIP: Scaffolding of basic Note class. Declaring __init__ method.

---
+++ python/notes-manager/note.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from typing import List, Optional
+import yaml
+
+class Note:
+    def __init__(self, title: str, content: str, tags: Optional[List[str]] = None, created: Optional[datetime] = None,
+                 author: Optional[str] = None, status: Optional[str] = None, priority: Optional[str] = None):
+        
+
+
+
+
+
+        return None
